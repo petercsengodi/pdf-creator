5e02c9d82759ab74dbec0520ae6ac8fbbe9f36c8
Peter Csengodi <pcsengodi@skawa.hu>
2017-10-09 16:19:12 +0200

    started pdf creator project

---
+++ PDFCreator/src/main/java/hu/csega/pdf/PDFWriter.java
@@ -0,0 +1,5 @@
+package hu.csega.pdf;
+
+public class PDFWriter {
+
+}
---
+++ PDFCreator/src/test/java/hu/csega/pdf/RunPDFWriter.java
@@ -0,0 +1,10 @@
+package hu.csega.pdf;
+
+public class RunPDFWriter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
